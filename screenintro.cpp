0b9c7da6352efb299285e1b26653c3fb26b65579
LINGXI-XPS\Lingxi <lingxizhong@gmail.com>
2017-12-03 15:44:54 -0700

    Added diagram map

--- screenintro.cpp
+++ screenintro.cpp
@@ -4,7 +4,7 @@ screenIntro::screenIntro(QWidget *parent) : QWidget(parent)
 {
     QLabel *intro = new QLabel(this);
     QVBoxLayout *boxLayout = new QVBoxLayout;
-    intro->setStyleSheet("QLabel { background-color : black; color : blue; }");
+    intro->setStyleSheet("QLabel { background-color : black; color : grey; border-color: white; border-style: outset; border-width: 2px; border-color: beige}");
     QFont tabFont;
 
     tabFont.setBold(true);
@@ -14,12 +14,19 @@ screenIntro::screenIntro(QWidget *parent) : QWidget(parent)
     intro->setFont(tabFont);
     intro->setText("Machine Learning");
     intro->resize(1000, 100);
-    intro->show();
 
 
     QLabel *info1 = new QLabel(this);
     info1->setText("Machine Learning is the process in which computers apply statistical learning techniques to identify patterns in data. This data can then be used to make highly accurate predictions. \nThis guide will go over Reinforcement Learning, a type of Machine Learning commonly used in games");
-    info1->show();
-    boxLayout->addWidget(intro);
+
     setLayout(boxLayout);
+    QPushButton *diagram = new QPushButton;
+    QPixmap pixDiagram("../cs3505-f17-a8-edu-app-matwilso/diagrams/rl1png.png");
+    QIcon ButtonIcon(pixDiagram);
+    diagram->setIcon(ButtonIcon);
+    diagram->setIconSize(pixDiagram.rect().size());
+
+    boxLayout->addWidget(intro);
+    boxLayout->addWidget(info1);
+    boxLayout->addWidget(diagram);
 }
