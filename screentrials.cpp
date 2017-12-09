c5bc4c47f18673011c0c2fe0c1adbb243f878c53
LINGXI-XPS\Lingxi <lingxizhong@gmail.com>
2017-12-09 12:17:02 -0700

    Added signal slots to screentrials

--- screentrials.cpp
+++ screentrials.cpp
@@ -27,26 +27,22 @@ screenTrials::screenTrials(QWidget *parent) : QWidget(parent)
     QLabel *info1 = new QLabel(this);
     QString text = "In reinforcement learning, the more trials you run, the more information that the model has to predict actions.";
     text+= "\n This means that the more trials you have the more accurate the model can be";
-    /*
-    text+= "This data can then be used to make highly accurate predictions.";
-    text+= "\nThis guide will go over Reinforcement Learning, a type of Machine Learning commonly used in games";
-    text+= "\n In reinforcement learning you have the agent do some action in the environment, and based on the action you give it rewards ";
-    text+= "\n ";
-    */
 
     info1->setText(text);
 
 
-    QLabel* gifSpot = new QLabel(this);
-    gifSpot->setText("LASDJFLASKDJFASDLKFJASLDKFJASLDKJFASLDKJF");
-
+    gif = new QLabel;
     //Adding to the layout
     girdLayout->addWidget(intro,   0,0,1,0);
     girdLayout->addWidget(info1,   1,0,1,1);
-    girdLayout->addWidget(gifSpot, 1,1,1,1);
+    girdLayout->addWidget(gif, 1,1,1,1);
     girdLayout->addWidget(choices, 2,0,1,2);
+    connect(choices, SIGNAL(activated(int)), this, SLOT(change(int)));
+}
 
-
-
-
+void screenTrials::change(int index) {
+    QMovie *movie = new QMovie("../cs3505-f17-a8-edu-app-matwilso/diagrams/trials100.gif");
+    gif->setMovie(movie);
+    movie->setSpeed(200);
+    movie->start();
 }
